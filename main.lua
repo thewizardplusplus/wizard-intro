e8732f39668d4e8f6f23e90398faf66627f3bee7
thewizardplusplus <thewizardplusplus@yandex.ru>
2024-07-17 11:23:35 +0500

    Improve the field positioning

--- main.lua
+++ main.lua
@@ -21,6 +21,8 @@ local width
 local height
 local field
 local cell_size
+local x_offset
+local y_offset
 local logo
 local total_dt
 
@@ -32,15 +34,24 @@ function _initialize_field(width, height)
     local cell_size = math.floor(min_dimension / MIN_SIDE_CELL_COUNT)
     local max_side_cell_count = math.floor(max_dimension / cell_size)
 
+    local min_side_offset = math.floor((min_dimension - MIN_SIDE_CELL_COUNT * cell_size) / 2)
+    local max_side_offset = math.floor((max_dimension - max_side_cell_count * cell_size) / 2)
+
     local field_size
+    local x_offset = 0
+    local y_offset = 0
     if is_album_orientation then
         field_size = Size:new(max_side_cell_count, MIN_SIDE_CELL_COUNT)
+        x_offset = max_side_offset
+        y_offset = min_side_offset
     else
         field_size = Size:new(MIN_SIDE_CELL_COUNT, max_side_cell_count)
+        x_offset = min_side_offset
+        y_offset = max_side_offset
     end
 
     local field = random.generate(Field:new(field_size), FIELD_FILLING)
-    return field, cell_size
+    return field, cell_size, x_offset, y_offset
 end
 
 function _initialize_logo(width, height, mode)
@@ -65,7 +76,7 @@ end
 function love.load()
     width = love.graphics.getWidth()
     height = love.graphics.getHeight()
-    field, cell_size = _initialize_field(width, height)
+    field, cell_size, x_offset, y_offset = _initialize_field(width, height)
     logo = _initialize_logo(width, height, "loading")
     total_dt = 0
 
@@ -90,8 +101,8 @@ function love.draw()
             return
         end
 
-        local x = point.x * cell_size + cell_size / 2
-        local y = point.y * cell_size + cell_size / 2
+        local x = point.x * cell_size + cell_size / 2 + x_offset
+        local y = point.y * cell_size + cell_size / 2 + y_offset
         local radius = (cell_size - cell_size * CELL_PADDING) / 2
 
         love.graphics.setColor({0.85, 0.85, 0.85})
@@ -109,7 +120,7 @@ end
 function love.resize(new_width, new_height)
     width = new_width
     height = new_height
-    field, cell_size = _initialize_field(width, height)
+    field, cell_size, x_offset, y_offset = _initialize_field(width, height)
     _initialize_logo(width, height, "resizing")
     total_dt = 0 -- force the start delay
 end
