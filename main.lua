57961a45ca34713661224aae3af6e06a49bebd3a
thewizardplusplus <thewizardplusplus@yandex.ru>
2024-07-17 10:57:55 +0500

    Draw the logo as PNG

--- main.lua
+++ main.lua
@@ -7,6 +7,7 @@ local Point = require("lualife.models.point")
 local Field = require("lualife.models.field")
 local random = require("lualife.random")
 local life = require("lualife.life")
+local center = require("center")
 
 local UPDATE_PERIOD = 0.25
 local START_DELAY = 1
@@ -14,11 +15,13 @@ local MIN_SIDE_CELL_COUNT = love.system.getOS() ~= "Android" and 30 or 25
 local FIELD_FILLING = 0.25
 local CELL_PADDING = 0.1
 local CELL_BORDER = 0.125
+local LOGO_PADDING = 0.1
 
 local width
 local height
 local field
 local cell_size
+local logo
 local total_dt
 
 function _initialize_field(width, height)
@@ -40,10 +43,30 @@ function _initialize_field(width, height)
     return field, cell_size
 end
 
+function _initialize_logo(width, height, mode)
+    local logo
+    if mode == "loading" then
+        logo = love.graphics.newImage("resources/logo.png")
+        center:setupScreen(logo:getPixelWidth(), logo:getPixelHeight())
+    elseif mode == "resizing" then
+        center:resize(width, height)
+    else
+        error("unknown mode of the logo initialization: " .. mode)
+    end
+
+    local min_dimension = math.min(width, height)
+    local logo_padding = min_dimension * LOGO_PADDING
+    center:setBorders(logo_padding, logo_padding, logo_padding, logo_padding)
+    center:apply()
+
+    return logo
+end
+
 function love.load()
     width = love.graphics.getWidth()
     height = love.graphics.getHeight()
     field, cell_size = _initialize_field(width, height)
+    logo = _initialize_logo(width, height, "loading")
     total_dt = 0
 
     love.mouse.setVisible(false)
@@ -77,12 +100,17 @@ function love.draw()
         love.graphics.setColor({1, 1, 1})
         love.graphics.circle("fill", x, y, radius - cell_size * CELL_BORDER)
     end)
+
+    center:start()
+    love.graphics.draw(logo, 0, 0)
+    center:finish()
 end
 
 function love.resize(new_width, new_height)
     width = new_width
     height = new_height
     field, cell_size = _initialize_field(width, height)
+    _initialize_logo(width, height, "resizing")
     total_dt = 0 -- force the start delay
 end
 
